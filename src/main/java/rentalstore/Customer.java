2461e76ad055c8197190a6f1e9d5a0e11c7edfeb
YANGCA <742305220@qq.com>
2018-08-27 19:33:31 +0800

    将customer中的statement方法和htmlStatement方法中最近租用影片积分计算提取成一个方法

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -20,64 +20,52 @@ public class Customer {
     }
 
     public String statement(){
-        int frequentRenterPoints = 0;
         Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while(rentals.hasMoreElements()){
-
             Rental each = (Rental) rentals.nextElement();
-
-            //add frequent renter points
-            frequentRenterPoints ++;
-            //add bonus for a two day new release rental
-            if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1){
-                frequentRenterPoints ++;
-            }
-
-            //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + getEachCharge(each) + "\n";
         }
-
-        //add footer lines
         result += "Amount owed is " + getTotalAmount() + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + getFrequentRenterPoints() + " frequent renter points";
         return result;
     }
 
     public String htmlStatement() {
-        int frequentRenterPoints = 0;
         Enumeration rentals = this.rentals.elements();
         String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
         while (rentals.hasMoreElements()) {
-
             Rental each = (Rental) rentals.nextElement();
-
-            //add frequent renter points
-            frequentRenterPoints ++;
-            //add bonus for a two day new release rental
-            if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1){
-                frequentRenterPoints ++;
-            }
-
-            //show figures for this rental
             result += each.getMovie().getTitle() + ":\t" + getEachCharge(each) + "<BR>\n";
         }
-        //add footer lines
         result += "<P>You owe<EM>" + getTotalAmount() + "</EM><P>\n";
-        result += "On this rental you earned <EM>" + frequentRenterPoints +
+        result += "On this rental you earned <EM>" + getFrequentRenterPoints() +
                 "</EM> frequent renter points<P>";
         return result;
     }
 
 
     public double getTotalAmount(){
-        double totalAmount = 0;
         Enumeration rentals = this.rentals.elements();
+        double totalAmount = 0;
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             totalAmount += getEachCharge(each);
         }
         return totalAmount;
+    }
+
+    public int getFrequentRenterPoints(){
+        Enumeration rentals = this.rentals.elements();
+        int frequentRenterPoints = 0;
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            frequentRenterPoints ++;
+            if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1){
+                frequentRenterPoints ++;
+            }
+        }
+        return frequentRenterPoints;
     }
 
     public double getEachCharge(Rental each){
